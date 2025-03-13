5dd6e81196567bde72424e57a95c9921e202248e
Saqlain.Iqbal <saqlainameer011@gmail.com>
2025-03-13 23:55:15 +0500

    updated chages .

--- JWTAuthDotNet8/Program.cs
+++ JWTAuthDotNet8/Program.cs
@@ -1,6 +1,9 @@
 using JWTAuthDotNet8.Data;
 using JWTAuthDotNet8.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+   .AddJwtBearer(options =>
+   {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         ValidIssuer = builder.Configuration.GetValue<string>("AppSettings:Issuer"),
+         ValidAudience = builder.Configuration.GetValue<string>("AppSettings:Audience"),
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("AppSettings:Token")!))
+      };
+   });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
